59b3bec1abceae9d01babb515c4467322a925988
Rohan Mehta <rhnmht30@gmail.com>
2023-05-01 13:46:43 +0530

    feat: add SSR support with supabase

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import React from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = React.useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
